218c35daa2fce2ab3131eb59733f45675204e854
Simon Schauß <simon.schauss@gmail.com>
2016-08-03 16:39:43 +0200

    fixed issue with proxy table

--- app.js
+++ app.js
@@ -4,22 +4,9 @@ const proxy = require('http-proxy-middleware');
 var app = express();
 var port = process.env.PORT || 3000
 var registerProxy = (table) => {
-    const context = table.context;
-    const options = {
-        target: table.target,
-        changeOrigin: true,
-        ws: true,
-        pathRewrite: {
-
-        },
-        proxyTable: {
-            [table.source]: table.target
-        }
-    };
-    const middleware = proxy(context, options);
-    app.use(middleware);
+    var middleware = proxy({target: table.target, changeOrigin: true});
+    app.use(table.source, middleware);
 };
-
 
 exports.run = (tables) => {
     tables.forEach(registerProxy);
