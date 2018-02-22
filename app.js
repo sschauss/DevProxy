509787a8168dc15623df3c096f6fbbc1c9d590db
Simon Schauß <simon.schauss@gmail.com>
2018-02-22 09:17:45 +0100

    add support for command line argument proxy mapping

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ const express = require('express');
 const proxy = require('http-proxy-middleware');
 
 const app = express();
-const port = process.env.PORT || 3000;
 const registerProxy = (table) => {
     const middleware = proxy(table.source, {target: table.target, changeOrigin: true, ws: table.ws || false});
     app.use(middleware);
     app.on('upgrade', middleware.upgrade);
 };
 
-exports.run = (tables) => {
+exports.run = (tables, port) => {
     tables.forEach(registerProxy);
     app.listen(port);
 };
--- main.js
+++ main.js
@@ -3,11 +3,22 @@
 const app = require('./app.js');
 
 const argv = process.argv.slice(2);
+const port = process.env.PORT || 3000;
+const cwd = process.cwd();
+let proxies;
 
-if (argv.length == 1) {
-    const proxies = require(`${__dirname}/${argv[0]}`);
-    app.run(proxies);
-} else {
-    console.log('Usage: yadp [filename]')
+switch (argv.length) {
+    case 1:
+        proxies = require(`${cwd}/${argv[0]}`);
+        app.run(proxies, port);
+        break;
+    case 2:
+        proxies = [{
+            source: "/",
+            target: `http://localhost:${argv[1]}`
+        }]
+        app.run(proxies, argv[0]);
+        break;
+    default:
+        console.log('Usage: yadp [filename]')
 }
-
